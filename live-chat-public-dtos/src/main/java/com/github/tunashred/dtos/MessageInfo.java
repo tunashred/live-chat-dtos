657e52c4254bc285349800a9d83b692d24a94417
tunashred <enachealexandru.traian@gmail.com>
2025-02-09 20:23:37 +0200

    Add public dtos

---
+++ live-chat-public-dtos/src/main/java/com/github/tunashred/dtos/MessageInfo.java
@@ -0,0 +1,17 @@
+package com.github.tunashred.dtos;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MessageInfo {
+    GroupChat groupChat;
+    User user;
+    String message;
+}
---
+++ live-chat-public-dtos/src/main/java/com/github/tunashred/dtos/User.java
@@ -0,0 +1,16 @@
+package com.github.tunashred.dtos;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    String name;
+    String userID;
+}
