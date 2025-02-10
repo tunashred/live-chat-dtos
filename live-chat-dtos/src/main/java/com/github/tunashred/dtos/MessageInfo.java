a563417bb8721e880c94ff6d56a5b52e154a7c3e
tunashred <enachealexandru.traian@gmail.com>
2025-02-11 00:19:17 +0200

    Add (de)serialization static methods

--- live-chat-dtos/src/main/java/com/github/tunashred/dtos/MessageInfo.java
+++ live-chat-dtos/src/main/java/com/github/tunashred/dtos/MessageInfo.java
@@ -1,5 +1,7 @@
 package com.github.tunashred.dtos;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,4 +16,14 @@ public class MessageInfo {
     GroupChat groupChat;
     User user;
     String message;
+
+    static public String serialize(MessageInfo message) throws JsonProcessingException {
+        ObjectMapper writer = new ObjectMapper();
+        return writer.writeValueAsString(message);
+    }
+
+    static public MessageInfo deserialize(String messageSerialized) throws JsonProcessingException {
+        ObjectMapper reader = new ObjectMapper();
+        return reader.readValue(messageSerialized, MessageInfo.class);
+    }
 }
